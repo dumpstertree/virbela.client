4f9f901c7a566ddb8341d13be2e73ad2be3d56c3
Zachary Collins <zcollins@virbela.com>
2021-03-26 13:40:59 -0400

    initial commit

---
+++ Runtime/Client.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using TinyMessenger;
+using Virbela.Events;
+
+namespace Virbela {
+
+	public class Client {
+
+		[RuntimeInitializeOnLoadMethod]
+		public static void Init () {
+			new Client();
+		}
+
+		public Client () {
+
+			Debug.Log( "--Client Initialized--" );
+
+			TinyMessengerHub.Instance.Subscribe<ToggleChangedEvent>( HandleToggleChanged );
+			TinyMessengerHub.Instance.Subscribe<SliderChangedEvent>( HandleSliderChanged );
+			TinyMessengerHub.Instance.Subscribe<FieldChangedEvent>( HandleFieldChanged );
+		}
+
+
+		private TinyMessengerHub messageHub;
+
+		private void HandleToggleChanged ( ToggleChangedEvent evnt ) {
+
+			Debug.Log( $"Toggle was changed to a value of '{evnt.Value}'" );
+		}
+		private void HandleSliderChanged ( SliderChangedEvent evnt ) {
+
+			Debug.Log( $"Slider was changed to a value of '{evnt.Value}'" );
+		}
+		private void HandleFieldChanged ( FieldChangedEvent evnt ) {
+
+			Debug.Log( $"Field was changed to a value of '{evnt.Value}'" );
+		}
+	}
+}
